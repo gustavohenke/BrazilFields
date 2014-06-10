17420a51c13dfb0f0ed94c11ecf088451a9e9726
Gustavo Henke <gustavo@injoin.com.br>
2014-06-10 15:19:46 -0300

    test(brCpf): adiciona testes para a directive brCpf

---
+++ test/spec/cpf.directive.spec.js
@@ -0,0 +1,54 @@
+describe( "brCpf Directive", function() {
+	"use strict";
+	
+	var scope, input, ngModel;
+	var expect = chai.expect;
+	
+	beforeEach( module( "brazilfields" ) );
+	beforeEach( inject(function( $rootScope, $compile ) {
+		scope = $rootScope;
+		input = $compile( "<input type='text' ng-model='foo' br-cpf>" )( scope );
+		ngModel = input.controller( "ngModel" );
+	}));
+	
+	it( "não deve validar se o valor do atributo retorna false", function() {
+		input.attr( "br-cpf", "false" );
+		input.val( "20620614813" ).triggerHandler( "input" );
+		scope.$apply();
+
+		expect( ngModel.$error.cpf ).to.be.undefined;
+	});
+	
+	describe( "da view para o model", function() {
+		it( "deve setar a validação 'cpf' como válida quando valor é CPF válido", function() {
+			input.val( "20620614803" ).triggerHandler( "input" );
+			scope.$apply();
+			
+			expect( ngModel.$error.cpf ).to.not.be.ok;
+		});
+		
+		it( "deve setar a validação 'cpf' como válida quando valor é CPF válido", function() {
+			input.val( "20620614813" ).triggerHandler( "input" );
+			scope.$apply();
+	
+			expect( ngModel.$error.cpf ).to.be.ok;
+		});
+	});
+	
+	describe( "do model para a view", function() {
+		it( "deve setar a validação 'cpf' como válida quando valor é CPF válido", function() {
+			scope.foo = "20620614803";
+			scope.$apply();
+			
+			expect( ngModel.$error.cpf ).to.not.be.ok;
+		});
+		
+		it( "deve setar a validação 'cpf' como válida quando valor é CPF válido", function() {
+			scope.foo = "20620614813";
+			scope.$apply();
+	
+			expect( ngModel.$error.cpf ).to.be.ok;
+		});
+	});
+	
+});
